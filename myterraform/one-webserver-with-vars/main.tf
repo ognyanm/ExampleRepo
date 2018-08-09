c8a06c6566730339d8cbb5f46ac6740f2651393d
Ognyan Marinov <ognyanm@helecloud.com>
2018-08-09 14:36:05 +0300

    change of ubuntu ami type

--- myterraform/one-webserver-with-vars/main.tf
+++ myterraform/one-webserver-with-vars/main.tf
@@ -3,7 +3,7 @@ provider "aws" {
 }
 
 resource "aws_instance" "example" {
-  ami                    = "ami-40d28157"
+  ami                    = "ami-2a7d75c0"
   instance_type          = "t2.micro"
   vpc_security_group_ids = ["${aws_security_group.instance.id}"]
 
